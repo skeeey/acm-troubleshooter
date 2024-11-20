4cac39c5d64882be876202ee2973b5cee9832e9e
Wei Liu <liuweixa@redhat.com>
2024-11-20 13:38:58 +0800

    add build binary
    Signed-off-by: Wei Liu <liuweixa@redhat.com>

--- hack/build_binary.sh
+++ hack/build_binary.sh
@@ -13,10 +13,21 @@ rm -rf $ROOT_DIR/_output $ROOT_DIR/build $ROOT_DIR/dist $ROOT_DIR/troubleshooter
 
 mkdir -p $ROOT_DIR/_output
 
+# required files
+litellm_location=$(pip show litellm | grep Location | awk '{print $2}')
+unstructured_location=$(pip show unstructured | grep Location | awk '{print $2}')
+emoji_location=$(pip show emoji | grep Location | awk '{print $2}')
+
 # build binary
-pyinstaller --hidden-import=tiktoken_ext.openai_public --hidden-import=tiktoken_ext apps/troubleshooter.py
-mkdir -p ${ROOT_DIR}/dist/troubleshooter/_internal/litellm/llms/tokenizers
-echo "{}" > ${ROOT_DIR}/dist/troubleshooter/_internal/litellm/llms/tokenizers/anthropic_tokenizer.json
+pyinstaller --add-data "$litellm_location/litellm/llms/tokenizers/anthropic_tokenizer.json:./litellm/llms/tokenizers" \
+    --add-data "$unstructured_location/unstructured/nlp/english-words.txt:./unstructured/nlp" \
+    --add-data "$emoji_location/emoji/unicode_codes:./emoji/unicode_codes" \
+    --hidden-import=tiktoken_ext.openai_public \
+    --hidden-import=tiktoken_ext \
+    apps/troubleshooter.py
+
+# mkdir -p ${ROOT_DIR}/dist/troubleshooter/_internal/litellm/llms/tokenizers
+# echo "{}" > ${ROOT_DIR}/dist/troubleshooter/_internal/litellm/llms/tokenizers/anthropic_tokenizer.json
 
 pushd $ROOT_DIR/dist/troubleshooter
 tar -zcf $ROOT_DIR/_output/acm-troubleshooter-$(uname)-$(uname -m).tar.gz *
